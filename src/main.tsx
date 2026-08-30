724087bca295684dbe9386f4754dc7160e963036
priyesh rai <jagritip134@gmail.com>
2026-08-30 16:36:34 +0530

    Load CNC and bearing showcase styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles.css';
 import './animations.css';
+import './showcase.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
